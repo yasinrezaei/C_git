ebdf23aac1e075ce7a205d88b8d626d2df4e3152
yasinrezaei <yasinrezaei@hotmail.com>
2020-07-04 12:03:23 +0430

    start and create files

---
+++ functions.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+
+void testfunc(){
+    printf("test");
+}
---
+++ git.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include"headerfile.h"
+void main(){
+    testfunc();
+}
